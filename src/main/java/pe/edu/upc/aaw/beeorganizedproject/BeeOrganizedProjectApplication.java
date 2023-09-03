86d0cea97228b722b1fc42320320ec338ba18b63
Sebastian Viacava <matiasviacava23@gmail.com>
2023-09-03 12:33:19 -0500

    Se agrega la base del Proyecto

---
+++ src/main/java/pe/edu/upc/aaw/beeorganizedproject/BeeOrganizedProjectApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.aaw.beeorganizedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeOrganizedProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeOrganizedProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/aaw/beeorganizedproject/BeeOrganizedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.aaw.beeorganizedproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeOrganizedProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
