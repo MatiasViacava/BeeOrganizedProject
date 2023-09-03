2968861aebda4371ea3800b416375c38e34903eb
Sebastian Viacava <matiasviacava23@gmail.com>
2023-09-03 17:47:01 -0500

    Creacion del aparatdo Tipo de Usuario

---
+++ src/main/java/pe/edu/upc/aaw/beeorganizedproject/entities/TipoUsuario.java
@@ -0,0 +1,31 @@
+package pe.edu.upc.aaw.beeorganizedproject.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class TipoUsuario {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idTipoUsuario;
+    @Column(name = "tipoUsuario",nullable = false, length = 45)
+    private String tipoUsuario;
+
+    public TipoUsuario() {
+    }
+
+    public int getIdTipoUsuario() {
+        return idTipoUsuario;
+    }
+
+    public void setIdTipoUsuario(int idTipoUsuario) {
+        this.idTipoUsuario = idTipoUsuario;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+}
