b186398639906f0a774726ab58e4053c12f8f873
Sebastian Viacava <matiasviacava23@gmail.com>
2023-09-04 18:18:32 -0500

    Añadiendo los paquetes de Configuracion y dependencias

--- src/main/java/pe/edu/upc/aaw/beeorganizedproject/serviceimplements/ConfiguracionServiceImplement.java
+++ src/main/java/pe/edu/upc/aaw/beeorganizedproject/serviceimplements/ConfiguracionServiceImplement.java
@@ -1,8 +1,32 @@
 package pe.edu.upc.aaw.beeorganizedproject.serviceimplements;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pe.edu.upc.aaw.beeorganizedproject.entities.Configuracion;
+import pe.edu.upc.aaw.beeorganizedproject.entities.Idioma;
+import pe.edu.upc.aaw.beeorganizedproject.repositories.IConfiguracionRepository;
 import pe.edu.upc.aaw.beeorganizedproject.serviceinterfaces.IConfiguracionService;
+
+import java.util.List;
 
 @Service
 public class ConfiguracionServiceImplement implements IConfiguracionService {
+    @Autowired
+    private IConfiguracionRepository iC;
+    @Override
+    public void insert(Configuracion configuracion) {
+        iC.save(configuracion);
+    }
+    @Override
+    public List<Configuracion> list() {
+        return null;
+    }
+    @Override
+    public void delete(int idConfiguracion) {
+        iC.deleteById(idConfiguracion);
+    }
+    @Override
+    public Configuracion listId(int idConfiguracion) {
+        return iC.findById(idConfiguracion).orElse(new Configuracion());
+    }
 }
