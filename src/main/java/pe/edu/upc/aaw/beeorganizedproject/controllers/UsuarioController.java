463d72ca02c23f4acbee33d27ed47299f57ebc42
Sebastian Viacava <matiasviacava23@gmail.com>
2023-09-04 15:11:35 -0500

    Agregacion de los paquetes de Usuario

--- src/main/java/pe/edu/upc/aaw/beeorganizedproject/controllers/UsuarioController.java
+++ src/main/java/pe/edu/upc/aaw/beeorganizedproject/controllers/UsuarioController.java
@@ -1,4 +1,32 @@
 package pe.edu.upc.aaw.beeorganizedproject.controllers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.aaw.beeorganizedproject.dtos.UsuarioDTO;
+import pe.edu.upc.aaw.beeorganizedproject.entities.Usuario;
+import pe.edu.upc.aaw.beeorganizedproject.serviceinterfaces.IUsuarioService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+    @Autowired
+    private IUsuarioService uR;
+    @PostMapping
+    public void registrar(@RequestBody UsuarioDTO dto){
+        ModelMapper m = new ModelMapper();
+        Usuario i = m.map(dto, Usuario.class);
+        uR.insert(i);
+    }
+    @GetMapping
+    public List<UsuarioDTO> listar(){
+        return uR.list().stream().map(x->{
+            ModelMapper m = new ModelMapper();
+            return m.map(x, UsuarioDTO.class);
+        }).collect(Collectors.toList());
+    }
+
 }
--- src/main/java/pe/edu/upc/aaw/beeorganizedproject/dtos/UsuarioDTO.java
+++ src/main/java/pe/edu/upc/aaw/beeorganizedproject/dtos/UsuarioDTO.java
@@ -1,4 +1,80 @@
 package pe.edu.upc.aaw.beeorganizedproject.dtos;
 
+import pe.edu.upc.aaw.beeorganizedproject.entities.TipoUsuario;
+
+import java.time.LocalDate;
+
 public class UsuarioDTO {
+    private int idUsuario;
+    private String Nombres;
+    private String Apellidos;
+    private LocalDate FechaNacimiento;
+    private String Universidad;
+    private String Email;
+    private String Contraseña;
+    private TipoUsuario tipoUsuario;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombres() {
+        return Nombres;
+    }
+
+    public void setNombres(String nombres) {
+        Nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        Apellidos = apellidos;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return FechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        FechaNacimiento = fechaNacimiento;
+    }
+
+    public String getUniversidad() {
+        return Universidad;
+    }
+
+    public void setUniversidad(String universidad) {
+        Universidad = universidad;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getContraseña() {
+        return Contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        Contraseña = contraseña;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
 }
