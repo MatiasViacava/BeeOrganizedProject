df6152f1868d74791f3dd1d6ce96bedd1f66ce8e
JuanTF <117841978+JuanTF@users.noreply.github.com>
2023-09-03 22:08:32 -0500

    Añadir tabla TipoActividad

--- src/main/java/pe/edu/upc/aaw/beeorganizedproject/entities/TipoActividad.java
+++ src/main/java/pe/edu/upc/aaw/beeorganizedproject/entities/TipoActividad.java
@@ -1,4 +1,38 @@
 package pe.edu.upc.aaw.beeorganizedproject.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="TipoActividad")
 public class TipoActividad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int IDTipoActividad;
+
+    @Column(name = "NombreTipoActividad",nullable = false,length = 60)
+    private String NombreTipoActividad;
+
+    public TipoActividad(){
+    }
+
+    public TipoActividad(int IDTipoActividad, String nombreTipoActividad) {
+        this.IDTipoActividad = IDTipoActividad;
+        NombreTipoActividad = nombreTipoActividad;
+    }
+
+    public int getIDTipoActividad() {
+        return IDTipoActividad;
+    }
+
+    public void setIDTipoActividad(int IDTipoActividad) {
+        this.IDTipoActividad = IDTipoActividad;
+    }
+
+    public String getNombreTipoActividad() {
+        return NombreTipoActividad;
+    }
+
+    public void setNombreTipoActividad(String nombreTipoActividad) {
+        NombreTipoActividad = nombreTipoActividad;
+    }
 }
