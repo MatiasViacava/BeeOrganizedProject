c0dddab99c6d24aed51ffcda220094b2d48232d9
JulioDT28 <juliodiaztito02@gmail.com>
2023-09-06 18:51:56 -0500

    Revert "Seguridad"
    This reverts commit f5f510b714c5b015879dcf57ca9a777b66e9362d.

--- src/main/java/pe/edu/upc/aaw/beeorganizedproject/repositories/IUsuarioRepository.java
+++ src/main/java/pe/edu/upc/aaw/beeorganizedproject/repositories/IUsuarioRepository.java
@@ -2,8 +2,8 @@ package pe.edu.upc.aaw.beeorganizedproject.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pe.edu.upc.aaw.beeorganizedproject.entities.DatosUsuario;
+import pe.edu.upc.aaw.beeorganizedproject.entities.Usuario;
 
 @Repository
-public interface IUsuarioRepository extends JpaRepository<DatosUsuario, Integer> {
+public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
 }
